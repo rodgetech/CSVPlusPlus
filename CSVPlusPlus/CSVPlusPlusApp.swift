8b36e391020affa0603733599b172459b6d0b3b5
Luis <rodgetech@gmail.com>
2025-08-28 16:23:15 -0600

    Initial Commit

---
+++ CSVPlusPlus/CSVPlusPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSVPlusPlusApp.swift
+//  CSVPlusPlus
+//
+//  Created by Luis Rodriguez on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CSVPlusPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSVPlusPlusTests/CSVPlusPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  CSVPlusPlusTests.swift
+//  CSVPlusPlusTests
+//
+//  Created by Luis Rodriguez on 28/08/25.
+//
+
+import Testing
+@testable import CSVPlusPlus
+
+struct CSVPlusPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
